0ecd5f3429d85a235e1dd49ba2ac008a582b1624
karun-singh <karun.singh@datakaveri.org>
2023-01-16 15:22:23 +0530

    added build-push script

---
+++ docker/build-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MAJOR_RELEASE=4
+MINOR_RELEASE=5.0
+COMMIT_ID=`git log -1 --pretty=%h` # last commit-id in short form
+
+# To be executed from project root
+docker build -t ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID -f docker/depl.dockerfile . && \
+docker push ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID
+
+docker build -t ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID -f docker/dev.dockerfile . && \
+docker push ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID
