9600fdd86d3ecaed25b2cf939f2a407d00e2efd4
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2023-04-05 17:19:34 +0530

    upgrade docker tag to 5.0.0-alpha

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-MAJOR_RELEASE=4
-MINOR_RELEASE=5.0
+MAJOR_RELEASE=5
+MINOR_RELEASE=0
+PATCH_RELEASE=0-alpha
 COMMIT_ID=`git log -1 --pretty=%h` # last commit-id in short form
 
 # To be executed from project root
-docker build -t ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID -f docker/depl.dockerfile . && \
-docker push ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID
+docker build -t ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID -f docker/depl.dockerfile . && \
+docker push ghcr.io/datakaveri/rs-proxy-depl:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID
 
-docker build -t ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID -f docker/dev.dockerfile . && \
-docker push ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE-$COMMIT_ID
+docker build -t ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID -f docker/dev.dockerfile . && \
+docker push ghcr.io/datakaveri/rs-proxy-dev:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID
