d61fec3270a133ec4616f39e5e989319b58f3465
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2024-02-02 10:42:22 +0530

    Adding async search and async status queries in adapter code

--- examples/consumers(adapter)/rpc_adapter_pune_live.py
+++ examples/consumers(adapter)/rpc_adapter_pune_live.py
@@ -1,6 +1,7 @@
 import json
 import re
 import logging
+import random
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 import pika
@@ -9,7 +10,7 @@ from datetime import datetime, timedelta
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-#ElasticSearchLogHandler is to handle the logs from elk client. so that we can access the status codes
+#To handle the logs from elasticsearch client. So that we can access the status codes
 class ElasticSearchLogHandler(logging.Handler):
     def __init__(self):
         super().__init__()
@@ -21,17 +22,16 @@ class ElasticSearchLogHandler(logging.Handler):
             # Clear the logs list before appending new log messages
             self.logs.clear()
             self.logs.append(log_message)
-        #print("Logs so far:", self.logs)
 
 # Instantiate the ElasticSearchLogHandler
-elasticsearch_log_handler = ElasticSearchLogHandler()
+elk_logHandler = ElasticSearchLogHandler()
 
 # Set the logging level of the ElasticSearchLogHandler to INFO
-elasticsearch_log_handler.setLevel(logging.INFO)
+elk_logHandler.setLevel(logging.INFO)
 
 # Configure the logging system
 logger = logging.getLogger()
-logger.addHandler(elasticsearch_log_handler)
+logger.addHandler(elk_logHandler)
 logger.setLevel(logging.INFO)
 
 class RabbitMqServerConfigure:
@@ -71,20 +71,20 @@ class RabbitmqServer:
         logging.info("Message is published")
 
 class ElasticsearchDataHandler:
-    def __init__(self, config, elasticsearch_log_handler):
+    def __init__(self, config, elk_logHandler):
         self.config = config
-        self.elasticsearch_log_handler = elasticsearch_log_handler
+        self.elk_logHandler = elk_logHandler
         # Configure logging for Elasticsearch client
         elasticsearch_logger = logging.getLogger('elasticsearch.trace')
         logging.info(elasticsearch_logger.handlers)  # Check if ElasticSearchLogHandler is present among the handlers
         elasticsearch_logger.setLevel(logging.INFO)
-        # Add the Elastic-Search Log Handler to the Elasticsearch logger
-        elasticsearch_logger.addHandler(elasticsearch_log_handler)
+        # Add the custom handler to the Elasticsearch logger
+        elasticsearch_logger.addHandler(elk_logHandler)
 
     def search_pune_flood_data(self, json_object, query, rout_key, corr_id, method):
         elk_config = self.config['elasticsearch']
         client = Elasticsearch(
-                    [f"http://{elk_config['databaseIP']}:{elk_config['databasePort']}"],
+                    [f"http://{elk_config['databaseURI']}:{elk_config['databasePort']}"],
                     basic_auth=(elk_config['databaseUser'], elk_config['databasePassword'])
                 )
         index_name = elk_config['index_name']
@@ -92,8 +92,40 @@ class ElasticsearchDataHandler:
         offset = None
         limit = json_object.get('limit')  # Get limit value from JSON request
         offset = json_object.get('offset')  # Get offset value from JSON request
-
-        if query:
+        #integrating the adapter code with async search and status APIs
+        apiEndpoint = json_object.get('api')
+        if apiEndpoint == '/ngsi-ld/v1/async/search':
+            response_payload = {
+                              "searchId": "58d3d383-6392-433f-b383-80d8e10be7e9",
+                              "statusCode": 201
+                              }
+        elif apiEndpoint == '/ngsi-ld/v1/async/status':
+            searchId = json_object.get("searchId")
+            # Randomly choose between  (complete) and  (in-progress)
+            if random.choice([True, False]):
+                                response_payload = {
+                                    "statusCode": 200,
+                                    "results": [
+                                    {
+                                    "status": "COMPLETE",
+                                    "progress": 100,
+                                    "file-download-url": "https://example.com/filename",
+                                    "searchId": searchId
+                                    }
+                                    ]
+                                }
+            else:
+                response_payload = {
+                                    "statusCode": 200,
+                                    "results": [
+                                    {
+                                           "status": "IN_PROGRESS",
+                                           "progress": 70,
+                                           "searchId": searchId
+                                            }
+                                    ]
+                 }
+        elif query:
             if "options" in json_object and json_object["options"] == "count":
                 # Convert query to a dictionary
                 query_dict = query.to_dict()
@@ -101,27 +133,28 @@ class ElasticsearchDataHandler:
                 # Define the Elasticsearch query
                 if limit is not None and offset is not None:
                     query = {
-                       "query": {
-                           "bool": {
-                               "must": [
-                                   query_dict
-                               ]
-                           }
-                       }
-                       ,
-                       "size": int(limit),  # Equivalent to limit
-                       "from": int(offset)   # Equivalent to offset
-                    }
+                                        "query": {
+                                            "bool": {
+                                                "must": [
+                                                    query_dict
+                                                ]
+                                            }
+                                        }
+                                        ,
+                                        "size": int(limit),  # Equivalent to limit
+                                        "from": int(offset)   # Equivalent to offset
+                                    }
                 else:
                     query = {
-                       "query": {
-                           "bool": {
-                              "must": [
-                                  query_dict
-                              ]
+                           "query": {
+                                "bool": {
+                                     "must": [
+                                              query_dict
+                                              ]
+                                            }
+                                        }
+
                            }
-                       }
-                    }
                 count_response = client.count(index=index_name, body=query)
                 logging.info(count_response)
                 count = count_response['count']
@@ -168,21 +201,26 @@ class ElasticsearchDataHandler:
                         }
         else:
             logging.info("Empty query")
-
-        #logging.info("Number of logs: %s", len(elasticsearch_log_handler.logs))
-        # Extract relevant information from captured log messages of elk client
-        for log in elasticsearch_log_handler.logs:
-            logging.info(log)
-            # Extract status code from log message using regular expression
-            status_code_match = re.search(r'status:(\d+)', log)
-            if status_code_match:
-                status_code = int(status_code_match.group(1))
-                print("Status Code:", status_code)
-            else:
-                logging.info("Not matched..")
+        flag = False
+        if( apiEndpoint !='/ngsi-ld/v1/async/search' and apiEndpoint !='/ngsi-ld/v1/async/status' ):
+            #logging.info("Number of logs: %s", len(elk_logHandler.logs))
+            # Extract relevant information from captured log messages of elk client
+            flag = True
+            for log in elk_logHandler.logs:
+                logging.info(log)
+                # Extract status code from log message using regular expression
+                status_code_match = re.search(r'status:(\d+)', log)
+                if status_code_match:
+                    status_code = int(status_code_match.group(1))
+                    print("Status Code:", status_code)
+                else:
+                    logging.info("Not matched..")
 
         if response_payload:
-            response_payload["statusCode"] = status_code  # Include the status code in the response payload
+            if( flag ):
+                response_payload["statusCode"] = status_code  # Include the status code in the response payload
+            logging.info("Adapter response: ")
+            #logging.info(response_payload)
             server.publish(response_payload, rout_key, corr_id, method)
 
         logging.info("Query Completed for Pune-Flood data")
@@ -191,46 +229,51 @@ def process_request(ch, method, properties, body):
     logging.info("Request JSON received")
     logging.info("Received request with body: %s", body)
     json_object = json.loads(body)
-    search_types = json_object['searchType'].split('_')
     rout_key = properties.reply_to
     corr_id = properties.correlation_id
-    elasticsearch_log_handler = ElasticSearchLogHandler()
-    pune_flood_db_search = ElasticsearchDataHandler(config=config, elasticsearch_log_handler=elasticsearch_log_handler)
-    # Remove 'latestSearch' from search_types if it exists
-    if 'latestSearch' in search_types:
-        search_types.remove('latestSearch')
-        logging.info("**Removed LatestSearch!!**")
+    elk_logHandler = ElasticSearchLogHandler()
+    pune_flood_db_search = ElasticsearchDataHandler(config=config, elk_logHandler=elk_logHandler)
+    #Async status
+    apiEndpoint = json_object.get('api')
+    #logging.info("api.."+apiEndpoint)
+    if apiEndpoint == '/ngsi-ld/v1/async/status':
+        searchId = json_object.get("searchId")
+        pune_flood_db_search.search_pune_flood_data(json_object, None, rout_key, corr_id, method)
 
-    if len(search_types) == 1:
-        search_type = search_types[0]
-        if search_type == 'temporalSearch':
-            temporal_query = build_temporal_query(json_object.get('temporal-query'))
-            pune_flood_db_search.search_pune_flood_data(json_object, temporal_query, rout_key, corr_id, method)
-        elif search_type == 'attributeSearch':
-            attribute_query = build_attribute_query(json_object.get('attr-query'))
-            # Add time range query
-            time_range_query = build_temporal_query({"time":"2020-10-12T00:00Z","endtime":"2020-10-22T00:00Z","timerel":"during"})
-            combined_query = build_combined_query(time_range_query, attribute_query, None)
-            pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
-        elif search_type == 'geoSearch':
-            logging.info(json_object.get('geo-query'))
-            geo_query = build_geo_query(json_object.get('geo-query'))
-            # Add time range query
-            time_range_query = build_temporal_query({"time":"2020-10-12T00:00Z","endtime":"2020-10-22T00:00Z","timerel":"during"})
-            combined_query = build_combined_query(time_range_query, None, geo_query)
-            pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
-        else:
-            logging.error("Unsupported searchType: %s", search_type)
-            return
     else:
-        #logging.info("Inside complex query...")
-        temporal_query = build_temporal_query(json_object.get('temporal-query'))
-        attribute_query = build_attribute_query(json_object.get('attr-query'))
-        geo_query = build_geo_query(json_object.get('geo-query'))
-        # Add time range query
-        #time_range_query = {"range": {"observationDateTime": {"gte": "2020-10-12T00:00Z", "lte": "2020-10-22T00:00Z"}}}
-        combined_query = build_combined_query(temporal_query, attribute_query, geo_query)
-        pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
+        search_types = json_object['searchType'].split('_')
+        # Remove 'latestSearch' from search_types if it exists
+        if 'latestSearch' in search_types:
+            search_types.remove('latestSearch')
+            #logging.info("**Removed LatestSearch!!**")
+
+        if len(search_types) == 1:
+            search_type = search_types[0]
+            if search_type == 'temporalSearch':
+                temporal_query = build_temporal_query(json_object.get('temporal-query'))
+                pune_flood_db_search.search_pune_flood_data(json_object, temporal_query, rout_key, corr_id, method)
+            elif search_type == 'attributeSearch':
+                attribute_query = build_attribute_query(json_object.get('attr-query'))
+                # Add time range query
+                time_range_query = build_temporal_query({"time":"2020-10-12T00:00Z","endtime":"2020-10-22T00:00Z","timerel":"during"})
+                combined_query = build_combined_query(time_range_query, attribute_query, None)
+                pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
+            elif search_type == 'geoSearch':
+                logging.info(json_object.get('geo-query'))
+                geo_query = build_geo_query(json_object.get('geo-query'))
+                # Add time range query
+                time_range_query = build_temporal_query({"time":"2020-10-12T00:00Z","endtime":"2020-10-22T00:00Z","timerel":"during"})
+                combined_query = build_combined_query(time_range_query, None, geo_query)
+                pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
+            else:
+                logging.error("Unsupported searchType: %s", search_type)
+                return
+        else:
+            temporal_query = build_temporal_query(json_object.get('temporal-query'))
+            attribute_query = build_attribute_query(json_object.get('attr-query'))
+            geo_query = build_geo_query(json_object.get('geo-query'))
+            combined_query = build_combined_query(temporal_query, attribute_query, geo_query)
+            pune_flood_db_search.search_pune_flood_data(json_object, combined_query, rout_key, corr_id, method)
 
 #temporal query
 def build_temporal_query(temporal_query_params):
@@ -272,7 +315,6 @@ def build_after_query(temporal_query_params):
 
 #Attribute Query
 def build_single_attribute_query(condition):
-    logging.info("am here in single attr query method..")
     parts = condition.split('==')
     if len(parts) == 2:
         # Equality condition
@@ -311,7 +353,6 @@ def build_attribute_query(attribute_query_params):
     attr_query = attribute_query_params
 
     if ';' in attr_query:
-        logging.info("am here in multiple attr method..")
         # Multiple conditions in attribute query separated by ';'
         conditions = attr_query.split(';')
         must_queries = []
@@ -376,15 +417,15 @@ def build_combined_query(temporal_query, attribute_query, geo_query):
     combined_query = Q('bool')
 
     if temporal_query:
-        logging.info("in complex temp...")
+        #logging.info("in complex temp...")
         combined_query &= temporal_query
 
     if attribute_query:
-        logging.info("in complex attribute_query...")
+        #logging.info("in complex attribute_query...")
         combined_query &= attribute_query
 
     if geo_query:
-        logging.info("in complex geo_query...")
+        #logging.info("in complex geo_query...")
         combined_query &= geo_query
     logging.info(combined_query)
     return combined_query
